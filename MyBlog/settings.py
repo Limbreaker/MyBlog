b5ec2eaf119c13e9336394bd2af48203b507f1c3
Limb <alexandrmaksimchuck@gmail.com>
2018-08-12 21:06:33 +0300

    add commit system

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'alexandrmaksimchuck@gmail.com@'
+EMAIL_HOST_USER = 'alexandrmaksimchuck@gmail.com'
 EMAIL_HOST_PASSWORD = 'nagatouzumaki98'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
