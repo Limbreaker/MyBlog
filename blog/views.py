591b27a9a909ffe87e67001c5efe40e66d81af61
Limb <alexandrmaksimchuck@gmail.com>
2018-08-14 20:33:22 +0300

    add tag-system and user-template

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,17 @@ from .models import Post, Comment
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
@@ -17,7 +24,8 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     return render(request, 'post/list.html', {'posts': posts,
-                                              'page': page})
+                                              'page': page,
+                                              'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -33,9 +41,11 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
             comment_form = CommentForm()
-
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request, 'post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment,
-                                                'comment_form': comment_form})
+                                                'comment_form': comment_form, 'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
